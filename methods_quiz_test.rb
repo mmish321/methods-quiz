ae0e26856a001b7ce8b0336ce7314d6559ea30a7
Michelle Mao <maom@s.dcsdk12.org>
2015-09-02 10:19:03 -0600

    Tests for icy_hot

--- methods_quiz_test.rb
+++ methods_quiz_test.rb
@@ -17,7 +17,15 @@ class MethodsQuizTest < MiniTest::Test
 		assert_equal false, @m.has_teen(4,3,2)
 		assert_equal true, @m.has_teen(19,15,14)
 	end
-	def not_string
+	def test_not_string
 		assert_equal "notcool", @m.not_string("cool")
 		assert_equal "nothing", @m.not_string("nothing")
+	end
+	def test_icy_hot
+		assert_equal true, @m.icy_hot(-79,6)
+		assert_equal true, @m.icy_hot(-100,189)
+		assert_equal false, @m.icy_hot(45,68)
+		assert_equal true, @m.icy_hot(8,108)
+		
+	end
 end
