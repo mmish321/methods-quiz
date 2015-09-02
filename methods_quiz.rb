027345b589a1bc7f900c4332f73722c27ccbaeba
Michelle Mao <maom@s.dcsdk12.org>
2015-09-02 10:08:41 -0600

    has_teen complete

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,6 +1,20 @@
 module MethodsQuiz
 	
-	# TODO - write has_teen?
+	def has_teen(number1, number2, number3)
+		if number1 >= 13 || number2 >= 13 || number3 >=13
+			if number1 <= 19
+				true
+			elsif number2 <= 19
+				true
+			elsif number3 <= 19
+				true
+			else
+				false
+			end
+		else
+			false
+		end
+	end
 	
 	# TODO - write not_string
 	
